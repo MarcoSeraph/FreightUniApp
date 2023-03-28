ceb2975688fc8096ce28b9ec54ebd9b4fbfccf12
MarcoSeraph <chenliibob@163.com>
2023-03-28 19:04:39 +0800

    commit

--- app/main.py
+++ app/main.py
@@ -1,24 +1,14 @@
 import os
+
 from flask import Flask
 
-from app import config
-from app.extensions import db
-from app.blueprints.Unallocated import bp as unallocated_bp
-from flask_migrate import Migrate
-
 app = Flask(__name__)
-# 绑定配置文件
-app.config.from_object(config)
-# 意思是在没有配置文件的时候先初始化
-db.init_app(app)
-Migrate = Migrate(app, db)
-# 绑定蓝图
-app.register_blueprint(unallocated_bp)
-
-# 设置config.py中配置信息连接数据库
-
-# 视图函数全部放在蓝图当中
 
 
-if __name__ == '__main__':
+@app.route('/')
+def hello_world():
+    return '欢迎使用微信云托管！'
+
+
+if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
